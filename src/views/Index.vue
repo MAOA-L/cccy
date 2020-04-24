85d9a25b2a807215889ab007c1652eeaadcc3e22
CyanZoy <nb1099312914@163.com>
2020-04-24 14:08:28 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+    <van-grid :column-num="3" clickable="clickable">
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="公交" to="/bus"/>
+    </van-grid>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Grid, GridItem } from 'vant';
+
+    Vue.use(Grid);
+    Vue.use(GridItem);
+    export default {}
+</script>
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+</style>
