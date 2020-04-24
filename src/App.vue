a4508c38cc1a8db9829e8e7191e2389f7617aad6
CyanZoy <nb1099312914@163.com>
2020-04-24 18:53:11 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--      <router-link to="/bus">BBBus</router-link>-->
+    <div id="app">
+        <van-nav-bar
+                title="标题"
+                left-text="返回"
+                right-text="按钮"
+                left-arrow
+                @click-left="onClickLeft"
+                @click-right="onClickRight"
+        />
+<!--        <div id="nav">-->
+<!--            &lt;!&ndash;      <router-link to="/">Home</router-link> |&ndash;&gt;-->
+<!--            &lt;!&ndash;      <router-link to="/about">About</router-link>&ndash;&gt;-->
+<!--            &lt;!&ndash;      <router-link to="/bus">BBBus</router-link>&ndash;&gt;-->
+<!--        </div>-->
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
+<script>
+    import Vue from 'vue';
+    import {NavBar, Toast} from 'vant';
 
+    Vue.use(NavBar);
+    Vue.use(Toast);
+
+    export default {
+        methods: {
+            onClickLeft() {
+                Toast('返回');
+            },
+            onClickRight() {
+                Toast('按钮');
+            },
+        },
+    };
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #nav {
+        padding: 30px;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
